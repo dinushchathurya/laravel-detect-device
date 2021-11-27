b62ebe500ce7597c532163a0b2e82a1c02161d63
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-27 17:29:39 +0530

    feat: Create route endpoint for device based result

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeviceController;
 
 Route::get('/', [DeviceController::class, 'detectDevice']);
+Route::get('/devices', [DeviceController::class, 'detectDevices']);
 
