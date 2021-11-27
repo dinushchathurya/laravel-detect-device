72551f131e67204218c5a0dadd9de39c7c47da46
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-27 17:29:06 +0530

    feat: Create another method to show result based on device

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use Jenssegers\Agent\Agent;
 
 class DeviceController extends Controller
-{
+{   
+    /* return view based on device*/
     public function detectDevice()
     {
         $agent = new Agent();
@@ -25,5 +26,29 @@ class DeviceController extends Controller
         if ($desktopDevice) {
             return view('welcome');
         }
+    }
+
+    /* return result based on device*/
+    public function detectDevices() 
+    {
+        $agent = new Agent();
+
+        $mobileDevice = $agent->isMobile();
+        if ($mobileDevice) {
+            $result = 'This is Mobile';
+        }
+
+        $tabletDevice = $agent->isTablet();
+        if ($tabletDevice) {
+            $result = 'This is Tablet';
+        }
+
+        $desktopDevice = $agent->isDesktop();
+        if ($desktopDevice) {
+            $result = 'This is Dekstop';
+        }
+
+        return view('devices',compact('result'));
+
     }
 }
